af86279c0a81777ccfa5c632cb039f52ee832cd8
Lucas Gabriel <gabriellucaswbtrombone@gmail.com>
2026-06-26 13:56:12 -0300

    Inicializando projeto

---
+++ AplicacaoDeConhecimento/Program.cs
@@ -0,0 +1,27 @@
+﻿class Filme
+{
+    public string Titulo { get; set; }
+    public int ClassificacaoIndicativa { get; set; }
+
+
+    public Filme (string titulo, int classificacaoIndicativa)
+    {
+        Titulo = titulo;
+        ClassificacaoIndicativa = classificacaoIndicativa;
+    }
+
+    public bool PodeAssistir(int idade)
+    {
+        return idade >= ClassificacaoIndicativa;
+    }
+
+    public void ExibirResultado (int idadeUsuario)
+    {
+        if (PodeAssistir(idadeUsuario))
+        {
+            Console.WriteLine($"Usuário pode assistir o filme {Titulo}.");
+        } else {
+            Console.WriteLine($"\nO usuário não pode assistir o filme {Titulo} pois é menor que a idade recomendada.");
+        }
+    }
+}
