9c564209764a2a436d8f547223adfc13dc9b7888
Lucas Gabriel <gabriellucaswbtrombone@gmail.com>
2026-06-26 16:22:17 -0300

    Iniciando o comando

--- AplicacaoDeConhecimento/CodeFile1.cs
+++
@@ -1,3 +0,0 @@
-﻿Filme filme = new Filme("O Poderoso chefão", 18);
-filme.ExibirResultado(20); // Usuário pode assistir o filme O Poderoso chefão
-filme.ExibirResultado(16); // O usuário não pode assistir o filme O Poderoso chefão pois é menor que a idade recomendada
