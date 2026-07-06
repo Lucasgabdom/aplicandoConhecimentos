1973b7fd06dbe04ed828f7cbc17212b2c3e19a60
Lucas Gabriel <gabriellucaswbtrombone@gmail.com>
2026-07-06 14:11:46 -0300

    Exercicio 11 finalizado

---
+++ 10-Colaboradores/Program.cs
@@ -0,0 +1,41 @@
+﻿public class Funcionario
+{
+    public string Nome { get; set; }
+    private double salario { get; set; }
+
+    public Funcionario(string nome, double salario)
+    {
+        Nome = nome;
+        this.salario = salario;
+    }
+}
+
+public class Gerente : Funcionario
+{
+    public string Setor { get; set; }
+
+    public Gerente (string nome, double salario, string setor) : base("Daniel", 8000)
+    {
+        Setor = setor;
+    }
+}
+
+public class Programador : Funcionario
+{
+    public string Linguagem { get; set; }
+
+    public Programador (string nome, double salario, string linguagem) : base("Lucas Gabriel", 12.000)
+    {
+        Linguagem = linguagem;
+    }
+}
+
+public class Analista : Funcionario
+{
+    public string Ferramenta { get; set; }
+
+    public Analista (string nome, double salario, string ferramenta) : base("João", 6000)
+    {
+        Ferramenta = ferramenta;
+    }
+}
---
+++ 11-Banco/Dados.cs
@@ -0,0 +1,8 @@
+﻿Conta Cliente1 = new Conta("João", 1000.0);
+Cliente1.MostrarInfo();
+Cliente1.Depositar(500.0);
+Cliente1.MostrarInfo();
+Cliente1.Sacar(200.0);
+Cliente1.MostrarInfo();
+
+
