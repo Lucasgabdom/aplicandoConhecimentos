72f673bdebfe436311f7a7503c2b825b2e81f943
.\Lucasgabdom <gabriellucaswbtrombone@gmail.com.br>
2026-08-06 22:36:59 -0300

    Aprendendo sobre polimorfismo

---
+++ 39-RelatorioFuncionarios/Desenvolvedor.cs
@@ -0,0 +1,10 @@
+﻿public class Desenvolvedor : Funcionario
+{
+    public Desenvolvedor (string nome, string departamento) : base (nome, departamento) { }
+
+    public override void GerarRelatorio()
+    {
+        Console.WriteLine("\n---- Gerando Relatório -----");
+        Console.WriteLine($"A função do desenvolvedor {Nome}: escrever código e corrigir bugs.");
+    }
+}
---
+++ 39-RelatorioFuncionarios/Gerente.cs
@@ -0,0 +1,13 @@
+﻿public class Gerente : Funcionario
+{
+
+    public Gerente (string nome, string departamento) : base (nome, departamento){}
+
+
+    public override void GerarRelatorio()
+    {
+        Console.WriteLine("\n---- Gerando Relatório -----");
+        Console.WriteLine($"A função do Gerente {Nome} do departamento {Departamento} é supervisonar a equipe! ");
+        
+    }
+}
