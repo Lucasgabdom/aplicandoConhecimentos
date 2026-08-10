c4bf12aa6bebba8bbefe9b955d71004740b094d1
Lucas Gabriel <gabriellucaswbtrombone@gmail.com>
2026-08-10 13:51:56 -0300

    Aplica .gitignore e remove obj/bin do versionamento

---
+++ 37-CalculadoraComSobrecarga/Program.cs
@@ -0,0 +1,37 @@
+﻿public class Calculadora
+{
+    public double Calculo;
+
+    public Calculadora() { }
+
+    public void Somar (int numero1, int numero2)
+    {
+        Calculo = numero1 + numero2;
+        Console.WriteLine(Calculo);
+    }
+
+    public void Somar(int numero1, int numero2, int numero3)
+    {
+        Calculo = numero1 + numero2 + numero3;
+        Console.WriteLine(Calculo);
+    }
+
+    public void Somar(double numero1, double numero2)
+    {
+        Calculo = (double) numero1 + numero2;
+        Console.WriteLine(Calculo);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculadora cal = new Calculadora();
+
+        cal.Somar(10,20);
+        cal.Somar(10, 20, 30);
+        cal.Somar(3.5, 3.2);
+
+    }
+}
