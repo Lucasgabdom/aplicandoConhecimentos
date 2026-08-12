5c6c3d4d1b6fbd4f2d27379bfd27dcc797f9f755
.\Lucasgabdom <gabriellucaswbtrombone@gmail.com.br>
2026-08-12 19:48:40 -0300

    Remove pastas obj e bin do controle de versao

---
+++ 40-NotificacaoSitemas/SmsNotificacos.cs
@@ -0,0 +1,10 @@
+﻿using Sgit checkout -b producao origin/producaoystem;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _40_NotificacaoSitemas
+{
+    internal class SmsNotificacos
+    {
+    }
+}
