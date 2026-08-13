e660335e1b535dd2efe41d0c018ed84fac5298ca
.\Lucasgabdom <gabriellucaswbtrombone@gmail.com.br>
2026-08-12 22:34:05 -0300

    teste

---
+++ 43-ReservasExperiencias/Program.cs
@@ -0,0 +1,10 @@
+﻿List<Reserva> reserva = new List<Reserva>
+{
+    new ReservaPrencial("Passa Quatro"),
+    new ReservaOnline("Workshop de programação")
+};
+
+foreach (var feito in reserva)
+{
+    feito.Confirmar();
+}
---
+++ 43-ReservasExperiencias/ReservaPresencial.cs
@@ -0,0 +1,14 @@
+﻿public class ReservaPrencial : Reserva
+{
+    public string Nome {  get; set; }
+
+    public ReservaPrencial(string nome) {  Nome = nome; }
+
+    public override void Confirmar()
+    {
+        Console.WriteLine($"Confirmando reserva presencial: {Nome}");
+    }
+
+
+}
+
